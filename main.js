347d8ccbbfec76fdde140e75e5ee61d8ef501fb3
smiledeeplearing <smiledeeplearing@gmail.com>
2026-02-24 13:24:06 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// Add JS here
+const generateBtn = document.getElementById('generate');
+const numbersDiv = document.querySelectorAll('.number');
+
+generateBtn.addEventListener('click', () => {
+    const numbers = [];
+    while (numbers.length < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        if (!numbers.includes(randomNumber)) {
+            numbers.push(randomNumber);
+        }
+    }
+
+    numbers.sort((a, b) => a - b);
+
+    numbersDiv.forEach((numberDiv, index) => {
+        numberDiv.textContent = numbers[index];
+    });
+});
